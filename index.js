d740d68f64bc4674d8a74545b348999908cb113e
oe.marcos <marcos.ojeda.escalante@gmail.com>
2024-09-10 09:52:08 -0300

    endpoint /abracadabra/usuarios y

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
 
+const arregloUsuarios = {
+    usuarios: [
+        "Juan",
+        "Maria",
+        "Francisca",
+        "Pedro"
+    ]
+};
+
 app.use(express.static('public'));
+
+app.use('/abracadabra/juego/:usuario', (req, res, next) => {
+    const usuario = req.params.usuario;
+    const usuarioExiste = arregloUsuarios.usuarios.find((usuarioTemporal) => {
+        return usuarioTemporal == usuario;
+    });
+    if(usuarioExiste){
+        next();
+    }else{
+        res.sendFile(__dirname + '/public/template/who.html');
+    }
+});
+
+app.get('/abracadabra/usuarios', (req, res) => {
+    res.json(arregloUsuarios);
+});
+
+
 
 app.listen(3000);
